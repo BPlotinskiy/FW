6ad64e40894ff3aa0ba12bb86a5ac535b819caa0
BPlotinskiy <boris_plotinskii@mail.ru>
2022-11-30 18:25:02 +0700

    решение задачи

--- Work/Program.cs
+++ Work/Program.cs
@@ -1,1 +1,16 @@
 ﻿// Написать программу, которая из имеющегося массивастрок формирует массив из строк, длина которых меньше либо равна 3 символа.
+
+
+string[] array = {"helloy", "2", "world", "11", "/'"};
+var newArray = new string[array.Length];
+var Size = 0;
+
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        newArray[Size] = value;
+        Size++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, newArray, 0, Size));
